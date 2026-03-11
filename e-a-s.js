a95b1d63c2a580407946e30dce0c8b915d128ada
Christen <257850355+FromConcentrates@users.noreply.github.com>
2026-03-11 10:54:00 -0400

    Complete preliminary 16x16 grid
    The simple use of flex and flex-wrap on #Container enables the squares to be appended to the rows
    via javascript. JS has been utilized to create the divs for the flexbox grid, as per instruction.
    Styling of the CSS is left, and it needs to be tweaked so that the border of the top row isn't doubled.

---
+++ e-a-s.js
@@ -0,0 +1,15 @@
+let Container = document.querySelector('#Container');
+
+
+for (let i = 0; i < 16; i++) {
+    let row = document.createElement('div');
+    row.classList.add('row');
+    Container.appendChild(row);
+    for(let b = 0; b < 16; b++) {
+    let square = document.createElement('div');
+    square.classList.add('square');
+    row.appendChild(square);
+    }
+
+
+}
