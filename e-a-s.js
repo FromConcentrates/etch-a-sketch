73a63759af1d82c142ada053fafd70c8e9e767bd
Christen <257850355+FromConcentrates@users.noreply.github.com>
2026-03-11 13:16:24 -0400

    Add reset button with parametered input
    userInput function needs to rebuild grid with new number

--- e-a-s.js
+++ e-a-s.js
@@ -1,6 +1,7 @@
+
 let Container = document.querySelector('#Container');
 
-
+function defaultSquares() {
 for (let i = 0; i < 16; i++) {
     let row = document.createElement('div');
     row.classList.add('row');
@@ -16,5 +17,35 @@ for (let i = 0; i < 16; i++) {
         
     }
 
-
 }
+}
+
+defaultSquares();
+
+function userInput() {
+    Container.innerHTML = "";
+    let number = prompt("Choose the number of boxes", "50");
+    if (number > 100) {
+    alert("message here");
+    return;
+}
+    
+}
+const inputBtn = document.querySelector("#inputBtn");
+inputBtn.addEventListener('click', function() {
+    
+})
+
+
+
+/* Create a button
+Button prompts user for input of number of boxes
+That input is used in a temperate literal formula 
+the formula dynamically calculate box sizes via pixels
+box pixels are applied to width value in CSS
+
+ result = `square.width: 980/${number}px`;
+ */ 
+
+/* clear the Container, re-initiate the for-loops using the 
+inputted number of boxes, calculate the pixel width of each box */
