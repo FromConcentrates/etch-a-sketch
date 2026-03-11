409abc8330b48f2ceda6b30517d0cd89c9b1727e
Christen <257850355+FromConcentrates@users.noreply.github.com>
2026-03-11 11:50:06 -0400

    Add mouseover event listener to color squares on hover

--- e-a-s.js
+++ e-a-s.js
@@ -5,10 +5,15 @@ for (let i = 0; i < 16; i++) {
     let row = document.createElement('div');
     row.classList.add('row');
     Container.appendChild(row);
-    for(let b = 0; b < 16; b++) {
-    let square = document.createElement('div');
-    square.classList.add('square');
-    row.appendChild(square);
+
+        for(let b = 0; b < 16; b++) {
+        let square = document.createElement('div');
+        square.classList.add('square');
+        square.addEventListener('mouseover', function() {
+          square.classList.add('squareHover')  
+        })
+        row.appendChild(square);
+        
     }
 
 
